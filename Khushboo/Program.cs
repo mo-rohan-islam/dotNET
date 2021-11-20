ee3e623e3161f9c97c9ead4fe43cb300e98aef96
nagrohan726 <nag.rohan726@gmail.com>
2021-11-20 21:57:40 +0530

    Initiating repo

---
+++ Khushboo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Khushboo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    string love = "Rohan";
+            Console.WriteLine("Hello Khushboo!\n");
+	    Console.Write("Who Love you? ");
+	    Console.Write(love);
+	    Console.WriteLine($"{love} Loves You Khushboo.");
+        }
+    }
+}
