71478ee5341be530792fbbfde880c37b45c23fa6
nagrohan726 <nag.rohan726@gmail.com>
2021-11-21 12:40:10 +0530

    Setting up MyApp in .NET 6.0 and Visual Studio 2022

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,13 +1,3 @@
-﻿using System;
-
-namespace MyApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
