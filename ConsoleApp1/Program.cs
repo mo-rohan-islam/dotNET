3aea8e9229390960b579ba61b27baf638e835003
nagrohan726 <nag.rohan726@gmail.com>
2021-11-21 20:41:32 +0530

    Understanding .NET 5.0 and .NET 6.0 simultaneously

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            IfStatement();
+        }
+
+        static void IfStatement()
+        {
+            var num1 = 5;
+            var num2 = 6;
+
+            var expression = num1 + num2 > 10;
+            if (expression)
+                Console.WriteLine("Expression is True!");
+
+            var num3 = 4;
+
+            if ((num1 + num2 + num3 > 10) && (num1 > num2))
+            {
+                Console.Write($"{num1} + {num2} + {num3} > 10");
+                Console.WriteLine($"\t\t&& {num1} > {num2}");
+            }
+            else
+            {
+                Console.WriteLine("Either or Both conditions don't satisfy.");
+            }
+
+            if (
+                    (num1 + num2 + num3 > 5)
+
+                    ||
+
+                    (num1 > num2)
+               )
+
+            {
+                Console.Write($"{num1} + {num2} + {num3} > 5");
+                Console.WriteLine($"\t\tAND/OR\t\t{num1} > {num2}");
+            }
+            else
+            {
+                Console.WriteLine("None of the conditions satisfy.");
+            }
+        }
+    }
+}
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿using System;
+
+namespace MyApp
+{
+    /* public */ class Program
+    {
+        /* public */ static void Main(String[] args)
+        {
+            // Snippet1();
+
+            Snippet2();            
+        }
+
+        static void Snippet2()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            var now = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello {name}! on {now:d} at {now:t}!");
+            Console.WriteLine($"{Environment.NewLine}Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void Snippet1()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
